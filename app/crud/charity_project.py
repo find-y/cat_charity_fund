670b05e10611152ee88553e094c963dc391650f5
unknown <yasha1bz@gmail.com>
2024-09-06 07:02:02 +0400

    tsts

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from fastapi import HTTPException
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
