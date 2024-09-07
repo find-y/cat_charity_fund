9855354bf61588a5d89c85595f56324a48079cc2
unknown <yasha1bz@gmail.com>
2024-09-07 19:02:15 +0400

    tests fixes

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Type, TypeVar
+from typing import Any, Optional, Type, TypeVar
 
 from fastapi import HTTPException
 from fastapi.encoders import jsonable_encoder
--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,9 +1,3 @@
-from typing import Optional
-
-from fastapi import HTTPException
-from sqlalchemy.exc import NoResultFound
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.crud.base import CRUDBase
 from app.models.charity_project import CharityProject
 
